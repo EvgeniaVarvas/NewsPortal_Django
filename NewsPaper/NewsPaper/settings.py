cdfad0f8a722619347e90cc76f39edafa524ff9b
EvgeniaVarvas <evgeniavarvas@gmail.com>
2024-03-07 09:41:37 +0300

    delete_log

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -19,6 +19,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOGGING_DIR = BASE_DIR / 'logs'
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
