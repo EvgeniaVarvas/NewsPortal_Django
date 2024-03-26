4f5c6b792920b4ce175418991ea65d87ffb25751
EvgeniaVarvas <evgeniavarvas@gmail.com>
2024-03-26 09:47:31 +0300

    add_colortheme

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 
     'allauth.account.middleware.AccountMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'NewsPaper.urls'
